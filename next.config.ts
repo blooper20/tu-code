2af05366db3a98d3619755d21114837d329cbdee
blooper20 <yjw127588@gmail.com>
2026-02-02 09:57:18 +0900

    chore: exclude all wasm files from bundle to fit cloudflare limits

--- next.config.ts
+++ next.config.ts
@@ -20,13 +20,17 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
+    // @ts-expect-error - This is a valid Next.js config but types might be outdated
     outputFileTracingExcludes: {
       '*': [
+        '**/*.wasm',
+        '**/*.swc',
         'node_modules/@swc/core-linux-x64-gnu',
         'node_modules/@swc/core-linux-x64-musl',
         'node_modules/@esbuild/linux-x64',
         'node_modules/webpack',
         'node_modules/terser',
+        'node_modules/sharp',
       ],
     },
   },
